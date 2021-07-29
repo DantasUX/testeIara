a0cb80b952cd448ced1554a3d40b2c35e7a332ed
Fabio Dantas <fabio.designer.pb@gmail.com>
2021-07-29 00:55:19 -0300

    primeiro comit

---
+++ index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./src/app";
+import Microfone from "./src/microfone"
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
